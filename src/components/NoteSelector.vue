2e38544b4f39ce75d258c9419afbeca673e56915
antoniosjefferson <itsmehtone@gmail.com>
2022-08-08 11:35:53 -0700

    adds NoteEditor, NoteSelector and NoteSelectors component

---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,8 @@
+<script></script>
+
+<template>
+  <div class="note-selector active">
+    <p class="note-selector-title">First note...</p>
+    <p class="note-selector-timestamp">Timestamp here...</p>
+  </div>
+</template>
